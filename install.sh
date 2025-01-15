79f4db488ab6d894f8cab8113d39838237c4bc7e
Timarlay <timarlay@gmail.com>
2025-01-15 15:26:00 +0200

    add chmod to sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 mv ./random_imei.sh /lib/functions/random_imei.sh
-
+chmod +x /lib/functions/random_imei.sh
 echo "./lib/functions/random_imei.sh" | cat - /etc/rc.local > /tmp/rc.local.tmp
 mv /tmp/rc.local.tmp /etc/rc.local
